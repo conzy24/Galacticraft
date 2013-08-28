f02c919b6e2fbe9f50f389042d91ceb98b0d5cea
conzy24 <conzy24@yahoo.com>
2013-08-27 17:19:23 -0700

    More work on mars
    Mars is based off of moon, expect bugs

--- common/micdoodle8/mods/galacticraft/mars/mars.java
+++ common/micdoodle8/mods/galacticraft/mars/mars.java
@@ -1,10 +1,58 @@
-//todo add main mars coding
+package micdoodle8.mods.galacticraft.moon;
 
-public class mars{
-public mars(){
+import micdoodle8.mods.galacticraft.api.world.IMapObject;
+import micdoodle8.mods.galacticraft.api.world.IMoon;
+import micdoodle8.mods.galacticraft.api.world.IPlanet;
+import micdoodle8.mods.galacticraft.core.GalacticraftCore;
+import micdoodle8.mods.galacticraft.moon.client.GCMoonMapPlanet;
+import micdoodle8.mods.galacticraft.moon.dimension.GCMoonWorldProvider;
+import net.minecraft.world.WorldProvider;
 
-}
-public String getPlanetName(){
-return "mars";
-}
+public class marsy implements IPlanet
+{
+    private final IMapObject moon = new GCMoonMapPlanet();
+
+    @Override
+    public String getName()
+    {
+        return "Mars";
+    }
+
+    @Override
+    public boolean isReachable()
+    {
+        return true;
+    }
+
+    
+   
+    @Override
+    public IMapObject getMapObject()
+    {
+        return this.mars;
+    }
+
+    @Override
+    public boolean autoRegister()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean addToList()
+    {
+        return true;
+    }
+
+    @Override
+    public Class<? extends WorldProvider> getWorldProvider()
+    {
+        return GCMoonWorldProvider.class;
+    }
+
+    @Override
+    public int getDimensionID()
+    {
+        return GCMoonConfigManager.dimensionIDMoon;
+    }
 }
